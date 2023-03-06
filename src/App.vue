2c524917b8995c169f4bc4ec40f5a5e9137884af
DylanPerry5@gmail.com <DylanPerry5@gmail.com>
2023-03-05 22:16:25 -0500

    Updating to include math as a link

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
-        <RouterLink to="/math"> </RouterLink>
+        <RouterLink to="/math">Math</RouterLink>
       </nav>
     </div>
   </header>
